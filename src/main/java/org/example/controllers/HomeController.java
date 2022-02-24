44162524cd9c266d5381655020120b9b73f0bfac
ouima2021 <ouima2014@gmail.com>
2022-02-24 15:28:59 -0600

    connected image/css to html pages

--- src/main/java/org/example/controllers/HomeController.java
+++ src/main/java/org/example/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("index")
+@RequestMapping("")
 public class HomeController {
 
     @RequestMapping(" ")
